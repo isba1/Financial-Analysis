7d42d08c61c070e5996701f3709a90d3cb867255
Ishiban10 <marinishaan@gmail.com>
2022-04-16 21:09:43 -0700

    added more graphs but need to fix legend on last asset vx liabilities

--- FinAnalysis.py
+++ FinAnalysis.py
@@ -111,37 +111,63 @@ class Financial_Analysis:
 
     def revenue_historic(self):
         x = np.arange(len(self.total_revenue_historic_array))
-        #y = np.linspace(0, np.amax(self.total_revenue_historic_array) + 100000000, 8)
         fig, ax = plt.subplots()
         bars = ax.bar(x, self.total_revenue_historic_array)
         ax.set_title = "Historic Revenue"
         ax.set_xticks(x)
         ax.set_xticklabels(self.years_array)
-        #ax.set_yticks(y)
-        #ax.set_yticklabels(y)
-        ax.bar_label(bars)
+        ax.bar_label(bars, fontsize=8)
         plt.ylabel("Total Revenue ($)")
         plt.xlabel("Date")
+        fig.suptitle('Historic Revenue')
         plt.show()
 
     def historic_net_income(self):
         # return graph rather just the list
-        return self.net_income_historic_array
+        x = np.arange(len(self.net_income_historic_array))
+        fig, ax = plt.subplots()
+        bars = ax.bar(x, self.net_income_historic_array)
+        ax.set_xticks(x)
+        ax.set_xticklabels(self.years_array)
+        ax.bar_label(bars, fontsize=8)
+        plt.xlabel("Date")
+        plt.ylabel("Net Income ($)")
+        fig.suptitle("Historic Net Income")
+        plt.show()
 
     def historic_gross_profit(self):
         # return graph rather than just the list
-        return self.gross_profit_historic_array
+        x = np.arange(len(self.gross_profit_historic_array))
+        fig, ax = plt.subplots()
+        bars = ax.bar(x, self.gross_profit_historic_array)
+        ax.bar_label(bars, fontsize=8)
+        ax.set_xticks(x)
+        ax.set_xticklabels(self.years_array)
+        fig.suptitle("Historic Gross Profit")
+        plt.xlabel("Date")
+        plt.ylabel("Gross Profit ($)")
+        plt.show()
 
     def EPS(self):
-        return self.gross_profit / self.shares_outstanding
+        return round(self.gross_profit / self.shares_outstanding, 2)
 
     def EPS_historic(self):
         eps_historic = []
         for item in self.gross_profit_historic_array:
-            eps_historic.append(item / self.shares_outstanding)
+            rnd = round(item / self.shares_outstanding, 2)
+            eps_historic.append(rnd)
         self.eps_historic_array = np.array(eps_historic)
         # return graph instead of the array
-        return self.eps_historic_array
+        x = np.arange(len(self.eps_historic_array))
+        fig, ax = plt.subplots()
+        bars = ax.bar(x, self.eps_historic_array)
+        ax.set_xticks(x)
+        ax.set_xticklabels(self.years_array)
+        ax.bar_label(bars, fontsize=8)
+        plt.xlabel("Date")
+        plt.ylabel("Earnings Per Share (EPS)")
+        fig.suptitle("Historic Earnings Per Share (EPS)")
+        plt.show()
 
     def free_cash_flow(self):
         return self.operating_cash_flow - self.capital_expenditures
@@ -160,14 +186,49 @@ class Financial_Analysis:
 
     def historic_debt(self):
         # return graph rather than just the list
-        return self.debt_historic_array
+        x = np.arange(len(self.debt_historic_array))
+        fig, ax = plt.subplots()
+        bars = ax.bar(x, self.debt_historic_array)
+        ax.bar_label(bars, fontsize=8)
+        ax.set_xticks(x)
+        ax.set_xticklabels(self.years_array)
+        plt.ylabel("Debt ($)")
+        plt.xlabel("Date")
+        fig.suptitle("Historic Debt")
+        plt.show()
 
     def historic_cash_flow(self):
         # return graph rather than just the list
-        return self.operating_cash_flow_historic_array
+        x = np.arange(len(self.operating_cash_flow_historic_array))
+        fig, ax = plt.subplots()
+        bars = ax.bar(x, self.operating_cash_flow_historic_array)
+        ax.bar_label(bars, fontsize=8)
+        ax.set_xticks(x)
+        ax.set_xticklabels(self.years_array)
+        plt.xlabel("Date")
+        plt.ylabel("Operating Cash FLow ($)")
+        fig.suptitle("Historic Operating Cash Flow")
+        plt.show()
 
-    #def asset_historic_vs_liabilities_historic(self):
+    def asset_historic_vs_liabilities_historic(self):
         # return the graph of this
+        x = np.arange(len(self.years_array))
+        x_large = x * 10
+        fig, ax = plt.subplots()
+        w = 4
+        rect1 = ax.bar(x_large-w/2, self.total_assets_historic_array,width=4)
+        rect2 = ax.bar(x_large+w/2, self.total_liabilities_historic_array, width=4)
+        ax.bar_label(rect1, fontsize=4)
+        ax.bar_label(rect2, fontsize=4)
+        ax.set_xticks(x_large)
+        ax.set_xticklabels(self.years_array)
+        plt.xlabel("Date")
+        plt.ylabel("$")
+        # fix the legend placement using loc and bbox_to_anchor
+        ax.legend([rect1, rect2], ["Assets", "Liabilities"], loc="upper right", bbox_to_anchor=(1.2, 1.2))
+        fig.suptitle("Historic Assets vs Historic Liabilities")
+        fig.tight_layout()
+        plt.show()
 
 
 
@@ -177,7 +238,7 @@ class Financial_Analysis:
 
 
 
-FA = Financial_Analysis("IBM")
+FA = Financial_Analysis("AAPL")
 """"
 print(f"Current Ratio: {FA.current_ratio()}")
 print(f"Working Capital: {FA.working_capital()}")
@@ -197,4 +258,9 @@ print(f"Historic Cash Flow: {FA.historic_cash_flow()}")
 print(f"Historic Debt: {FA.historic_debt()}")
 """
 FA.revenue_historic()
-
+FA.historic_net_income()
+FA.historic_gross_profit()
+FA.EPS_historic()
+FA.historic_debt()
+FA.historic_cash_flow()
+FA.asset_historic_vs_liabilities_historic()
