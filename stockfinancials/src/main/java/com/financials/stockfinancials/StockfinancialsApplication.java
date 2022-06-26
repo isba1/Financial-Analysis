cead93fb85a1b150275d4bc8249c5f1ccf7d7f0d
Ishiban10 <marinishaan@gmail.com>
2022-06-26 12:16:35 -0700

    Final initial commit

---
+++ stockfinancials/src/main/java/com/financials/stockfinancials/StockfinancialsApplication.java
@@ -0,0 +1,13 @@
+package com.financials.stockfinancials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockfinancialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockfinancialsApplication.class, args);
+	}
+
+}
---
+++ stockfinancials/src/test/java/com/financials/stockfinancials/StockfinancialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financials.stockfinancials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockfinancialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
